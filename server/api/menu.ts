ddec3844e9de364da998ba8edda27df4dae3da18
sruusk <57395313+sruusk@users.noreply.github.com>
2025-02-10 22:43:45 +0200

    add error handling to api route

--- server/api/menu.ts
+++ server/api/menu.ts
@@ -6,6 +6,21 @@ export default defineEventHandler(async (event) => {
   if(city && campus) {
     filter['city'] = city as string;
     filter['campus'] = campus as string;
+  } else if (city || campus) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'You must provide both city and campus parameters or none at all',
+    });
   }
-  return await getMenus(filter);
+
+  const restaurants = await getMenus(filter);
+
+  if(restaurants.length === 0) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'No restaurants found with the given parameters',
+    });
+  }
+
+  return restaurants;
 });
